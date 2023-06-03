5354d074897ef29d66f684277563f5acb82eb2d0
Abaljerind <abaljerinda@gmail.com>
2023-06-03 10:50:47 +0700

    add fontFamily, colos, fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbh: ["Kumbh Sans"],
+      },
+      colors: {
+        darkCyan: "hsl(185, 75%, 39%)",
+        darkBlue: "hsl(229, 23%, 23%)",
+        grayishBlue: "hsl(227, 10%, 46%)",
+        darkGray: "hsl(0, 0%, 59%),",
+      },
+      fontSize: {
+        nameStats: "18px",
+      },
+    },
   },
   plugins: [],
 };
